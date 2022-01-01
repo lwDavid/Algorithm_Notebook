91e9b20504c042005e4425a2cd6d512922e2f463
David <47696114+lwDavid@users.noreply.github.com>
2022-01-01 22:00:04 +0800

    Update PAT_B1041.cpp

--- Codes/PAT_B1041.cpp
+++ Codes/PAT_B1041.cpp
@@ -1,23 +1,26 @@
+#define maxn 1000
 #include <stdio.h>
+struct Student {
+    long long id;
+    int examSeat;
+} testSeat[maxn];
 int main() {
-    int num, query, input;
-    long long number[1000];
-    int spot[1000][2];
+    int num, query, seat, examSeat, input;
+    long long id;
     if (scanf("%d", &num))
         ;
     for (int i = 0; i < num; i++) {
-        if (scanf("%lld %d %d", &number[i], &spot[i][0], &spot[i][1]))
+        if (scanf("%lld %d %d", &id, &seat, &examSeat))
             ;
+        testSeat[seat].id = id;
+        testSeat[seat].examSeat = examSeat;
     }
     if (scanf("%d", &query))
         ;
     for (int i = 0; i < query; i++) {
         if (scanf("%d", &input))
             ;
-        for (int j = 0; j < num; j++) {
-            if (spot[j][0] == input)
-                printf("%lld %d", number[j], spot[j][1]);
-        }
+        printf("%lld %d", testSeat[input].id, testSeat[input].examSeat);
         if (i != query - 1)
             printf("\n");
     }
