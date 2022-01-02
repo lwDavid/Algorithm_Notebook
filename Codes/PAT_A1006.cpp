eedae6a58e775995b2a40ab5639dd611254de081
David <47696114+lwDavid@users.noreply.github.com>
2022-01-02 10:10:57 +0800

    Create PAT_A1006.cpp

---
+++ Codes/PAT_A1006.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+struct record {
+    char ID[16];
+    int signin[3];
+    int signout[3];
+} temp, unlock, lock;
+int later(int a[3], int b[3]) {
+    if (a[0] > b[0])
+        return 1;
+    else if (a[0] == b[0] && a[1] > b[1])
+        return 1;
+    else if (a[0] == b[0] && a[1] == b[1] && a[2] > b[2])
+        return 1;
+    else
+        return 0;
+}
+int main() {
+    unlock.signin[0] = 24;
+    lock.signout[0] = -1;
+    int n;
+    if (scanf("%d", &n))
+        ;
+    while (n--) {
+        if (scanf("%s %d:%d:%d %d:%d:%d", temp.ID, &temp.signin[0],
+                  &temp.signin[1], &temp.signin[2], &temp.signout[0],
+                  &temp.signout[1], &temp.signout[2]))
+            ;
+        if (later(temp.signout, lock.signout))
+            lock = temp;
+        if (!later(temp.signin, unlock.signin))
+            unlock = temp;
+    }
+    printf("%s %s", unlock.ID, lock.ID);
+    return 0;
+}
