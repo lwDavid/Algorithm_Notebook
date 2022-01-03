e801a85f96678222d906392d170a8980e65c8b0e
David <47696114+lwDavid@users.noreply.github.com>
2022-01-03 14:51:33 +0800

    Create codeup_5901.cpp

---
+++ Codes/codeup_5901.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstring>
+const int maxn = 256;
+bool judge(char str[]) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+int main() {
+    char str[maxn];
+    while (gets_s(str)) {
+        bool flag = judge(str);
+        if (flag == true) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
+    }
+    return 0;
+}
