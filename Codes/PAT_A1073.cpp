2fcce9217f32eb5509a569350e482d6438b10e38
David <47696114+lwDavid@users.noreply.github.com>
2022-01-04 20:04:06 +0800

    Create PAT_A1073.cpp

---
+++ Codes/PAT_A1073.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int Epos = 0, exp, i = 0, len;
+    char str[10000];
+    while ((str[i] = getchar())) {
+        if (str[i] == 'E') {
+            Epos = i;
+            break;
+        }
+        i++;
+    }
+    len = strlen(str) - 4;
+    if (scanf("%d", &exp))
+        ;
+    if (str[0] == '-')
+        printf("-");
+    if (exp < 0) {
+        exp = -exp;
+        for (int i = 0; i < exp; i++) {
+            if (i == 1)
+                printf(".");
+            printf("0");
+        }
+        for (int i = 1; i < Epos; i++) {
+            if (str[i] == '.')
+                continue;
+            printf("%c", str[i]);
+        }
+    } else if (exp == 0) {
+        for (int i = 1; i < Epos; i++) {
+            printf("%c", str[i]);
+        }
+    } else if (exp >= len) {
+        for (int i = 1; i < Epos; i++) {
+            if (str[i] == '.')
+                continue;
+            printf("%c", str[i]);
+        }
+        exp -= len;
+        for (int i = 0; i < exp; i++) {
+            printf("0");
+        }
+    } else {
+        int dot = 3 + exp;
+        for (int i = 1; i < Epos; i++) {
+            if (str[i] == '.') {
+                continue;
+            }
+            if (i == dot)
+                printf(".");
+            printf("%c", str[i]);
+        }
+    }
+    return 0;
+}
