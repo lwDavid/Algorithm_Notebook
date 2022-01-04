a4abdbe07b783b6aa51a82422f99f8e2d6612fd4
David <47696114+lwDavid@users.noreply.github.com>
2022-01-04 22:01:35 +0800

    Update PAT_B1048.cpp

--- Codes/PAT_B1048.cpp
+++ Codes/PAT_B1048.cpp
@@ -2,41 +2,34 @@
 #include <string.h>
 char odd[13] = {'0', '1', '2', '3', '4', '5', '6',
                 '7', '8', '9', 'J', 'Q', 'K'};
+void reverse(char str[]) {
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) {
+        char t = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 - i] = t;
+    }
+}
 int main() {
-    int len1, len2, lmax, count = 1;
-    char str1[101], str2[101], str1v[101], str2v[101], res[101];
+    int len1 = 0, len2 = 0, lmax, count = 1;
+    char str1[101] = {0}, str2[101] = {0}, res[101] = {0};
     if (scanf("%s %s", str1, str2))
-        ;
-    len1 = strlen(str1), len2 = strlen(str2);
-    for (int i = 0; i < len1; i++) {
-        str1v[len1 - 1 - i] = str1[i];
-    }
-    for (int i = 0; i < len2; i++) {
-        str2v[len2 - 1 - i] = str2[i];
-    }
+        len1 = strlen(str1), len2 = strlen(str2);
     lmax = len1 > len2 ? len1 : len2;
-    if (len1 > len2) {
-        for (int i = len2; i < lmax; i++) {
-            str2v[i] = '0';
-        }
-    } else if (len1 < len2) {
-        for (int i = len1; i < lmax; i++) {
-            str1v[i] = '0';
-        }
-    }
-    for (int i = 0; i < lmax; i++) {
-        if (count % 2 == 0) {
-            if (str2v[i] - str1v[i] >= 0)
-                res[i] = str2v[i] - str1v[i] + '0';
-            else
-                res[i] = str2v[i] - str1v[i] + 10 + '0';
-        } else {
-            res[i] = odd[(str1v[i] - '0' + str2v[i] - '0') % 13];
-        }
-        count++;
-    }
-    for (int i = lmax - 1; i >= 0; i--) {
-        printf("%c", res[i]);
-    }
+    reverse(str1), reverse(str2);
+    if (len1 > len2)
+        for (int i = len2; i < lmax; i++)
+            str2[i] = '0';
+    else
+        for (int i = len1; i < lmax; i++)
+            str1[i] = '0';
+    for (int i = 0; i < lmax; i++, count++)
+        if (count % 2 == 0)
+            res[i] = (str2[i] - str1[i] >= 0) ? str2[i] - str1[i] + '0'
+                                              : str2[i] - str1[i] + 10 + '0';
+        else
+            res[i] = odd[(str1[i] - '0' + str2[i] - '0') % 13];
+    reverse(res);
+    printf("%s", res);
     return 0;
 }
