e79a7f2e65157754cb30fee931dcee0115e66f2e
David <47696114+lwDavid@users.noreply.github.com>
2022-01-05 00:02:02 +0800

    Update PAT_A1005.cpp

--- Codes/PAT_A1005.cpp
+++ Codes/PAT_A1005.cpp
@@ -2,25 +2,21 @@
 #include <string.h>
 char spell[10][6] = {"zero", "one", "two",   "three", "four",
                      "five", "six", "seven", "eight", "nine"};
+void dfs(int n) {
+    if (n / 10 == 0) {
+        printf("%s", spell[n % 10]);
+        return;
+    }
+    dfs(n / 10);
+    printf(" %s", spell[n % 10]);
+}
 int main() {
-    int sum = 0, count = 0;
-    char str[102], res[4] = {0};
+    int sum = 0;
+    char str[102];
     if (scanf("%s", str))
         ;
     for (int i = 0; i < strlen(str); i++)
         sum += str[i] - '0';
-    if (sum == 0) {
-        printf("zero");
-        return 0;
-    }
-    do {
-        res[count++] = '0' + sum % 10;
-        sum /= 10;
-    } while (sum != 0);
-    for (int i = strlen(res) - 1; i >= 0; i--) {
-        printf("%s", spell[res[i] - '0']);
-        if (i != 0)
-            printf(" ");
-    }
+    dfs(sum);
     return 0;
 }
