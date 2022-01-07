ea8200f823bb61bfd57dc523cd85c487fa6fd096
David <47696114+lwDavid@users.noreply.github.com>
2022-01-08 04:20:51 +0800

    Update PAT_A1025.cpp

--- Codes/PAT_A1025.cpp
+++ Codes/PAT_A1025.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 struct Student {
     char number[14];
-    int f_rank, location, l_rank, score;
+    int location, l_rank, score;
 } list[30000];
 bool cmp(Student A, Student B) {
     if (A.score != B.score)
         return A.score > B.score;
     else
-        return strcmp(A.number, B.number)<0;
+        return strcmp(A.number, B.number) < 0;
 }
 int main() {
-    int N, K, count = 0, locat = 1, head, end,t;
+    int N, K, count = 0, locat = 1, head, end, t, rank;
     if (scanf("%d", &N))
         ;
     while (N--) {
@@ -26,9 +26,9 @@ int main() {
             count++;
         }
         end = count;
-        sort(list+head, list+end, cmp);
-        list[head].l_rank = 1,t=1;
-        for (int i = head+1; i < end; i++) {
+        sort(list + head, list + end, cmp);
+        list[head].l_rank = 1, t = 1;
+        for (int i = head + 1; i < end; i++) {
             if (list[i].score == list[i - 1].score)
                 list[i].l_rank = list[i - 1].l_rank;
             else
@@ -37,16 +37,16 @@ int main() {
         }
         locat++;
     }
-    sort(list, list+count, cmp);
+    sort(list, list + count, cmp);
     printf("%d\n", count);
-    list[0].f_rank = 1;
+    rank = 1;
     for (int i = 1; i < count; i++) {
-        if (list[i].score == list[i - 1].score)
-            list[i].f_rank = list[i - 1].f_rank;
-        else
-            list[i].f_rank = i + 1;
-        printf("%s %d %d %d", list[i].number, list[i].f_rank, list[i].location,list[i].l_rank);
-        if(i!=count-1) printf("\n");
+        if (list[i].score != list[i - 1].score)
+            rank = i + 1;
+        printf("%s %d %d %d", list[i].number, rank, list[i].location,
+               list[i].l_rank);
+        if (i != count - 1)
+            printf("\n");
     }
     return 0;
 }
