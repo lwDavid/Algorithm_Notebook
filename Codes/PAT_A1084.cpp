133a7c675da1a8cb63fbbfe54a50dc8cb05c8f1b
David <47696114+lwDavid@users.noreply.github.com>
2022-01-12 12:08:52 +0800

    Update PAT_A1084.cpp

--- Codes/PAT_A1084.cpp
+++ Codes/PAT_A1084.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 int hash(char t) {
     if (t >= '0' && t <= '9')
         return t - '0';
@@ -11,21 +11,22 @@ int hash(char t) {
         return 36;
 }
 int main() {
-    int len1 = 0, len2 = 0, hashTable[37] = {0};
+    int len1 = 0, len2 = 0;
+    bool hashTable[37] = {false};
     char str1[81], str2[81];
     if (scanf("%s", str1))
         len1 = strlen(str1);
     if (scanf("%s", str2))
         len2 = strlen(str2);
     for (int i = 0; i < len2; i++)
-        hashTable[hash(str2[i])] = 1;
+        hashTable[hash(str2[i])] = true;
     for (int i = 0; i < len1; i++)
-        if (hashTable[hash(str1[i])] == 0) {
+        if (!hashTable[hash(str1[i])]) {
             if (str1[i] >= 'a' && str1[i] <= 'z')
                 printf("%c", str1[i] - 'a' + 'A');
             else
                 printf("%c", str1[i]);
-            hashTable[hash(str1[i])] = 1;
+            hashTable[hash(str1[i])] = true;
         }
     printf("\n");
     return 0;
