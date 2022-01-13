450b74176261a3d6b50e5682c3a63c3a79fd60c9
David <47696114+lwDavid@users.noreply.github.com>
2022-01-13 19:36:46 +0800

    Update PAT_A1050.cpp

--- Codes/PAT_A1050.cpp
+++ Codes/PAT_A1050.cpp
@@ -17,3 +17,21 @@ int main() {
     printf("\n");
     return 0;
 }
+
+//I think the C codes below are also correct, but it is weird that they can not pass the checkpoint 1 & 3.
+// #include <stdio.h>
+// #include <string.h>
+// int main() {
+//     int valid[256] = {0}, len = 0, k = 0;
+//     char str[10001], c;
+//     while ((c = getchar()) != '\n')
+//         str[k++] = c;
+//     len = strlen(str);
+//     while ((c = getchar()) != '\n') 
+//         valid[(int)c] = 1;
+//     for (int i = 0; i < len; i++)
+//         if (!valid[(int)str[i]])
+//             printf("%c", str[i]);
+//     printf("\n");
+//     return 0;
+// }
