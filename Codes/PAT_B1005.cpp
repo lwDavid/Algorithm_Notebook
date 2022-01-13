39c0d1db9448215ea9fc6597614f191700010da6
David <47696114+lwDavid@users.noreply.github.com>
2022-01-14 01:55:43 +0800

    Create PAT_B1005.cpp

---
+++ Codes/PAT_B1005.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int K = 0, num = 0, tag[3967] = {0}, exist[101] = {0}, count = 0;
+void Callatz(int k) {
+    while (k != 1) {
+        tag[k]++;
+        if (k % 2 == 0)
+            k /= 2;
+        else
+            k = (3 * k + 1) / 2;
+    }
+    return;
+}
+int main() {
+    if (scanf("%d", &K))
+        for (int i = 0; i < K; i++)
+            if (scanf("%d", &num)) {
+                exist[num]++;
+                Callatz(num);
+            }
+    for (int i = 2; i < 101; i++)
+        if (tag[i] == 1 && exist[i])
+            count++;
+    for (int i = 100; i > 1; i--)
+        if (tag[i] == 1 && exist[i]) {
+            printf("%d", i);
+            count--;
+            if (count != 0)
+                printf(" ");
+        }
+    printf("\n");
+    return 0;
+}
