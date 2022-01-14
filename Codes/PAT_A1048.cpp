32610dfda8aa8a69a1860f795a531f028592a6e8
David <47696114+lwDavid@users.noreply.github.com>
2022-01-14 19:04:47 +0800

    Update PAT_A1048.cpp

--- Codes/PAT_A1048.cpp
+++ Codes/PAT_A1048.cpp
@@ -5,17 +5,13 @@ int main() {
         for (int i = 0; i < N; i++)
             if (scanf("%d", &coin))
                 coins[coin]++;
-    for (i = 1; i < (M - i); i++)
+    for (i = 1; i < M; i++)
         if (coins[i] && coins[M - i]) {
+            if((i==(M-i))&&coins[i]<2)
+                continue;
             printf("%d %d\n", i, M - i);
             return 0;
         }
-    if (i == (M - i) && coins[i] > 1) {
-        printf("%d %d\n", i, i);
-        return 0;
-    }
     printf("No Solution\n");
     return 0;
 }
-
-//This problem has a bug. In checkpoint 3, there are coins which have a face value more than 500.
