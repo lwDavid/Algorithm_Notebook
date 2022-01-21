4b32b932b36a83d3b5b6b186f1fff87b10e218d4
David <47696114+lwDavid@users.noreply.github.com>
2022-01-21 16:29:23 +0800

    Update PAT_A1067.cpp

--- Codes/PAT_A1067.cpp
+++ Codes/PAT_A1067.cpp
@@ -1,27 +1,23 @@
 #include <stdio.h>
-int count = 0;
-void swap_0(int pos[]) {
-    while (pos[0]) {
-        int temp = pos[0];
-        pos[0] = pos[pos[0]];
-        pos[temp] = temp;
-        count++;
-    }
-}
 int main() {
-    int N, pos[100000] = {0}, t;
+    int N, pos[100000] = {0}, t, count = 0;
     if (scanf("%d", &N))
         for (int i = 0; i < N; i++)
             if (scanf("%d", &t))
                 pos[t] = i;
-    swap_0(pos);
-    for (int i = 1; i < N; i++)
-        if (pos[i] != i) {
+    for (int i = 0; i < N; i++) {
+        if (pos[i] != i && i > 0) {
             pos[0] = pos[i];
             pos[i] = 0;
             count++;
-            swap_0(pos);
         }
+        while (pos[0]) {
+            int temp = pos[0];
+            pos[0] = pos[pos[0]];
+            pos[temp] = temp;
+            count++;
+        }
+    }
     printf("%d\n", count);
     return 0;
 }
