467950627c6c9a5e8618d2629568f1d07f793852
David <47696114+lwDavid@users.noreply.github.com>
2022-01-21 16:03:46 +0800

    Update PAT_A1067.cpp

--- Codes/PAT_A1067.cpp
+++ Codes/PAT_A1067.cpp
@@ -1,31 +1,27 @@
 #include <stdio.h>
 int count = 0;
-void swap_0(int arr[], int pos[]) {
-    while (arr[0]) {
-        arr[pos[0]] = pos[0];
+void swap_0(int pos[]) {
+    while (pos[0]) {
+        int temp = pos[0];
         pos[0] = pos[pos[0]];
-        pos[arr[pos[0]]] = arr[pos[0]];
-        arr[pos[0]] = 0;
+        pos[temp] = temp;
         count++;
     }
 }
 int main() {
-    int N, arr[100000] = {0}, pos[100000] = {0};
+    int N, pos[100000] = {0}, t;
     if (scanf("%d", &N))
         for (int i = 0; i < N; i++)
-            if (scanf("%d", &arr[i]))
-                pos[arr[i]] = i;
-    swap_0(arr, pos);
-    for (int i = 1; i < N; i++) {
-        if (arr[i] != i) {
-            arr[0] = arr[i];
-            pos[0] = i;
-            pos[arr[i]] = 0;
-            arr[i] = 0;
+            if (scanf("%d", &t))
+                pos[t] = i;
+    swap_0(pos);
+    for (int i = 1; i < N; i++)
+        if (pos[i] != i) {
+            pos[0] = pos[i];
+            pos[i] = 0;
             count++;
-            swap_0(arr, pos);
+            swap_0(pos);
         }
-    }
     printf("%d\n", count);
     return 0;
 }
