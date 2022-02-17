6903e7e0aeb9f638813f7bf387113f8585ef2e97
David <47696114+lwDavid@users.noreply.github.com>
2022-02-17 12:03:03 +0800

    Update PAT_A1048.cpp

--- Codes/PAT_A1048.cpp
+++ Codes/PAT_A1048.cpp
@@ -15,3 +15,37 @@ int main() {
     printf("No Solution\n");
     return 0;
 }
+
+//Another version using binary search.
+// #include <algorithm>
+// #include <cstdio>
+// using namespace std;
+// int N, M, coins[100001] = {0};
+// int Binary_Search(int L, int R, int x) {
+//     int left = L, right = R, mid;
+//     while (left < right) {
+//         mid = (left + right) / 2;
+//         if (coins[mid] > x)
+//             right = mid;
+//         else
+//             left = mid + 1;
+//     }
+//     return left;
+// }
+// int main() {
+//     if (scanf("%d %d", &N, &M))
+//         for (int i = 1; i <= N; i++)
+//             if (scanf("%d", &coins[i]))
+//                 ;
+//     sort(coins + 1, coins + N + 1);
+//     if (N > 1)
+//         for (int i = 1; i <= N; i++) {
+//             int j = Binary_Search(i, N + 1, M - coins[i]);
+//             if (coins[i] + coins[j - 1] == M && i != j - 1) {
+//                 printf("%d %d\n", coins[i], coins[j - 1]);
+//                 return 0;
+//             }
+//         }
+//     printf("No Solution\n");
+//     return 0;
+// }
