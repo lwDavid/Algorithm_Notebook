734c8650ad9947b5500e391cc33a2f70eb0d3a4d
David <47696114+lwDavid@users.noreply.github.com>
2022-02-18 17:14:39 +0800

    Update PAT_B1045.cpp

--- Codes/PAT_B1045.cpp
+++ Codes/PAT_B1045.cpp
@@ -2,30 +2,25 @@
 #include <cstdio>
 int main() {
     int num[100010], rightMin[100010], temp[100010];
-    int leftMax, N, i, j, count = 0;
+    int N, j, count = 0;
     if (scanf("%d", &N))
         for (int i = 0; i < N; i++)
             if (scanf("%d", &num[i]))
                 ;
-    leftMax = 0;
-    rightMin[N - 1] = 0x7fffffff;
-    i = 0, j = N - 2;
-    for (; j >= 0; j--)
+    for (j = N - 2, rightMin[N - 1] = 0x7fffffff; j >= 0; j--)
         rightMin[j] = std::min(num[j + 1], rightMin[j + 1]);
-    for (; i < N; i++) {
+    for (int i = 0, leftMax = 0; i < N; i++) {
         if (i)
             leftMax = std::max(num[i - 1], leftMax);
         if (num[i] < rightMin[i] && num[i] > leftMax)
             temp[count++] = num[i];
     }
-    std::sort(temp, temp + count);
-    if (!count)
-        printf("0\n\n");
-    else {
-        printf("%d\n%d", count, temp[0]);
-        for (int i = 1; i < count; i++)
-            printf(" %d", temp[i]);
-        printf("\n");
+    printf("%d\n", count);
+    for (int i = 0; i < count; i++) {
+        printf("%d", temp[i]);
+        if (i != count - 1)
+            printf(" ");
     }
+    printf("\n");
     return 0;
 }
