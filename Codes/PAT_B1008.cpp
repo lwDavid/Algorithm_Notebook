548b226d59cb2a0933683135781758979f22ddd2
David <47696114+lwDavid@users.noreply.github.com>
2022-02-20 17:08:47 +0800

    Update PAT_B1008.cpp

--- Codes/PAT_B1008.cpp
+++ Codes/PAT_B1008.cpp
@@ -15,3 +15,35 @@ int main() {
     }
     return 0;
 }
+
+//Another version that can rotate the array with O(1) extra space when input is the pointer to array.
+// #include <stdio.h>
+// int gcd(int a, int b) {
+//     return !b ? a : gcd(b, a % b);
+// }
+// int main() {
+//     int N, M, a[100];
+//     if (scanf("%d %d", &N, &M))
+//         for (int i = 0; i < N; i++)
+//             if (scanf("%d", &a[i]))
+//                 ;
+//     M %= N;
+//     if (M != 0 && N != 1) {
+//         int start = N - M, end = N - M + gcd(N, M) - 1;
+//         for (int i = start; i <= end; i++) {
+//             int temp = a[i], j = i;
+//             do {
+//                 a[j] = a[(j - M + N) % N];
+//                 j = (j - M + N) % N;
+//             } while (j != i);
+//             a[(i + M) % N] = temp;
+//         }
+//     }
+//     for (int i = 0; i < N; i++) {
+//         printf("%d", a[i]);
+//         if (i != N - 1)
+//             printf(" ");
+//     }
+//     printf("\n");
+//     return 0;
+// }
