999b32fad0a98f1800e372c28e40849604425c0d
David <47696114+lwDavid@users.noreply.github.com>
2022-02-20 16:18:59 +0800

    Update codeup_5901.cpp

--- Codes/codeup_5901.cpp
+++ Codes/codeup_5901.cpp
@@ -1,24 +1,19 @@
-#include <cstdio>
-#include <cstring>
-const int maxn = 256;
-bool judge(char str[]) {
-    int len = strlen(str);
-    for (int i = 0; i < len / 2; i++) {
-        if (str[i] != str[len - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
+#include <stdio.h>
+#include <string.h>
 int main() {
-    char str[maxn];
-    while (gets_s(str)) {
-        bool flag = judge(str);
-        if (flag == true) {
-            printf("Yes\n");
-        } else {
-            printf("No\n");
-        }
+    int i, len, flag;
+    char str[256];
+    while (scanf("%s", str) != EOF) {
+        len = strlen(str), flag = 1;
+        for (i = 0; i < len / 2; i++)
+            if (str[i] != str[len - 1 - i]) {
+                flag = 0;
+                break;
+            }
+        if (flag)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
