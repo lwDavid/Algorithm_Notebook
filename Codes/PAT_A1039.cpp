59564fcfaf94d4b718111576e687224a96fea2fb
David <47696114+lwDavid@users.noreply.github.com>
2022-03-02 02:01:51 +0800

    Update PAT_A1039.cpp

--- Codes/PAT_A1039.cpp
+++ Codes/PAT_A1039.cpp
@@ -56,3 +56,48 @@ int main() {
     }
     return 0;
 }
+
+// The reference version with worse time and memory consuming than mine.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// #include <vector>
+// using namespace std;
+// const int N = 40010;
+// const int M = 26 * 26 * 26 * 10;
+// vector<int> selectCourse[M];
+// int getID(char name[]) {
+//     int id = 0;
+//     for (int i = 0; i < 3; i++)
+//         id = id * 26 + (name[i] - 'A');
+//     id = id * 10 + (name[3] - '0');
+//     return id;
+// }
+// int main() {
+//     char name[5];
+//     int n, k;
+//     if (scanf("%d %d", &n, &k))
+//         ;
+//     for (int i = 0; i < k; i++) {
+//         int course, x;
+//         if (scanf("%d %d", &course, &x))
+//             ;
+//         for (int j = 0; j < x; j++) {
+//             if (scanf("%s", name))
+//                 ;
+//             int id = getID(name);
+//             selectCourse[id].push_back(course);
+//         }
+//     }
+//     for (int i = 0; i < n; i++) {
+//         if (scanf("%s", name))
+//             ;
+//         int id = getID(name);
+//         sort(selectCourse[id].begin(), selectCourse[id].end());
+//         printf("%s %d", name, (int)selectCourse[id].size());
+//         for (int j = 0; j < (int)selectCourse[id].size(); j++)
+//             printf(" %d", selectCourse[id][j]);
+//         printf("\n");
+//     }
+//     return 0;
+// }
