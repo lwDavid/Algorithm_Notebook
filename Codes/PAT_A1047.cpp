2d64599fbacc693f113b40ed4411e261c3c1b0f0
David <47696114+lwDavid@users.noreply.github.com>
2022-03-02 12:10:55 +0800

    Update PAT_A1047.cpp

--- Codes/PAT_A1047.cpp
+++ Codes/PAT_A1047.cpp
@@ -42,3 +42,36 @@ int main() {
     }
     return 0;
 }
+
+// The reference version which performs better in time consuming with small scale inputs than mine.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// #include <vector>
+// using namespace std;
+// const int maxn = 40010;
+// const int maxc = 2510;
+// char name[maxn][5];
+// vector<int> course[maxc];
+// bool cmp(int a, int b) {
+//     return strcmp(name[a], name[b]) < 0;
+// }
+// int main() {
+//     int n, k, c, courseID;
+//     if (scanf("%d %d", &n, &k))
+//         ;
+//     for (int i = 0; i < n; i++) {
+//         if (scanf("%s %d", name[i], &c))
+//             ;
+//         for (int j = 0; j < c; j++)
+//             if (scanf("%d", &courseID))
+//                 course[courseID].push_back(i);
+//     }
+//     for (int i = 1; i <= k; i++) {
+//         printf("%d %d\n", i, (int)course[i].size());
+//         sort(course[i].begin(), course[i].end(), cmp);
+//         for (int j = 0; j < (int)course[i].size(); j++)
+//             printf("%s\n", name[course[i][j]]);
+//     }
+//     return 0;
+// }
