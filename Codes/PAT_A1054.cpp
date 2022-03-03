96e72ff168a1f680ada21f77c3c976a834d5b210
David <47696114+lwDavid@users.noreply.github.com>
2022-03-03 18:24:58 +0800

    Update PAT_A1054.cpp

--- Codes/PAT_A1054.cpp
+++ Codes/PAT_A1054.cpp
@@ -1,18 +1,71 @@
-#include <iostream>
-#include <map>
-using namespace std;
+#include <stdio.h>
 int main() {
-    int M, N, max = 0;
-    string str, mark;
-    map<string, int> colors;
+    int M, N, count = 0, ans = -1, T, num;
     if (scanf("%d %d", &M, &N))
-        while (N--)
-            for (int i = 0; i < M; i++) {
-                cin >> str;
-                colors[str]++;
-                mark = colors[str] > max ? str : mark;
-                max = colors[str] > max ? colors[str] : max;
+        ;
+    T = M * N;
+    while (T--)
+        if (scanf("%d", &num)) {
+            if (num == ans)
+                count++;
+            else if (count > 0)
+                count--;
+            else {
+                ans = num;
+                count = 1;
             }
-    cout << mark << endl;
+        }
+    printf("%d\n", ans);
     return 0;
 }
+
+//My version using map.
+// #include <cstdio>
+// #include <map>
+// using namespace std;
+// int main() {
+//     int M, N, max = 0, half = 0, input, mark;
+//     map<int, int> colors;
+//     if (scanf("%d %d", &M, &N))
+//         half = M * N / 2;
+//     while (N--)
+//         for (int i = 0; i < M; i++) {
+//             if (scanf("%d", &input))
+//                 colors[input]++;
+//             mark = colors[input] > max ? input : mark;
+//             max = colors[input] > max ? colors[input] : max;
+//             if (max > half)
+//                 break;
+//         }
+//     printf("%d\n", mark);
+//     return 0;
+// }
+
+//The reference version.
+// #include <cstdio>
+// #include <map>
+// using namespace std;
+// int main() {
+//     int n, m, col;
+//     if (scanf("%d %d", &n, &m))
+//         ;
+//     map<int, int> count;
+//     for (int i = 0; i < n; i++)
+//         for (int j = 0; j < m; j++) {
+//             if (scanf("%d", &col)) {
+//                 if (count.find(col) != count.end())
+//                     count[col]++;
+//                 else
+//                     count[col] = 1;
+//             }
+//         }
+//     int k = 0, MAX = 0;
+//     for (map<int, int>::iterator it = count.begin(); it != count.end(); it++) {
+//         if (it->second > MAX) {
+//             k = it->first;
+//             MAX = it->second;
+//         }
+//     }
+//     printf("%d\n", k);
+//     return 0;
+// }
