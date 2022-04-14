b314c6c2dc770025d98a5baffa567880a95e29fe
David <47696114+lwDavid@users.noreply.github.com>
2022-04-14 15:23:42 +0800

    Create PAT_A1086.cpp

---
+++ Codes/PAT_A1086.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int n, temp, pre[31], in[31], pre_index = 1, in_index = 1;
+string str;
+stack<int> stk;
+struct node {
+    int lchild, rchild;
+} Nodes[31];
+int creat(int preL, int preR, int inL, int inR) {
+    if (preL > preR)
+        return -1;
+    int k = inL;
+    for (; k <= inR; k++)
+        if (in[k] == pre[preL])
+            break;
+    Nodes[pre[preL]].lchild = creat(preL + 1, preL + k - inL, inL, k - 1);
+    Nodes[pre[preL]].rchild = creat(preL + k - inL + 1, preR, k + 1, inR);
+    return pre[preL];
+}
+void postOrder(int root) {
+    if (root == -1)
+        return;
+    postOrder(Nodes[root].lchild);
+    postOrder(Nodes[root].rchild);
+    printf("%d", root), n--;
+    if (n != 0)
+        printf(" ");
+}
+int main() {
+    if (scanf("%d", &n))
+        for (int i = 0; i < 2 * n; i++) {
+            cin >> str;
+            if (str == "Push") {
+                if (scanf("%d", &temp)) {
+                    pre[pre_index] = temp;
+                    stk.push(temp);
+                    pre_index++;
+                }
+            } else {
+                int t = stk.top();
+                stk.pop();
+                in[in_index] = t;
+                in_index++;
+            }
+        }
+    postOrder(creat(1, n, 1, n));
+    printf("\n");
+    return 0;
+}
