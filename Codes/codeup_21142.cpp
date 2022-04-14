548e36966cd53d3efd4061d480133e0e1f20783e
David <47696114+lwDavid@users.noreply.github.com>
2022-04-14 13:06:17 +0800

    Create codeup_21142.cpp

---
+++ Codes/codeup_21142.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+priority_queue<long long, vector<long long>, greater<long long> > q;
+int main() {
+    int n;
+    long long temp, x, y, ans = 0;
+    if (scanf("%d", &n))
+        for (int i = 0; i < n; i++)
+            if (scanf("%lld", &temp))
+                q.push(temp);
+    while (q.size() > 1) {
+        x = q.top();
+        q.pop();
+        y = q.top();
+        q.pop();
+        q.push(x + y);
+        ans += x + y;
+    }
+    printf("%lld\n", ans);
+    return 0;
+}
