c8671fd4cd27cd9e93a2956d8003977fee399eb7
David <47696114+lwDavid@users.noreply.github.com>
2022-04-15 00:26:42 +0800

    Update PAT_A1090.cpp

--- Codes/PAT_A1090.cpp
+++ Codes/PAT_A1090.cpp
@@ -1,3 +1,4 @@
+//My version using a BFS algorithm with better memory consume but longer codes.
 #include <cmath>
 #include <cstdio>
 #include <queue>
@@ -43,5 +44,43 @@ int main() {
             }
     }
     BFS(root);
+    return 0;
+}
+
+//The reference DFS method with concise codes.
+#include <cmath>
+#include <cstdio>
+#include <vector>
+using namespace std;
+const int maxn = 100010;
+vector<int> child[maxn];
+double p, r;
+int n, maxDepth = 0, num = 0;
+void DFS(int index, int depth) {
+    if (child[index].size() == 0) {
+        if (depth > maxDepth) {
+            maxDepth = depth;
+            num = 1;
+        } else if (depth == maxDepth)
+            num++;
+        return;
+    }
+    for (int i = 0; i < (int)child[index].size(); i++)
+        DFS(child[index][i], depth + 1);
+}
+int main() {
+    int father, root = -1;
+    if (scanf("%d %lf %lf", &n, &p, &r))
+        r /= 100;
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &father))
+            ;
+        if (father != -1)
+            child[father].push_back(i);
+        else
+            root = i;
+    }
+    DFS(root, 0);
+    printf("%.2f %d\n", p * pow(1 + r, maxDepth), num);
     return 0;
 }
