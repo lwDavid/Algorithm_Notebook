ce95aeb37480f9aba54b3c2d677b643261d88e51
David <47696114+lwDavid@users.noreply.github.com>
2022-04-14 16:41:12 +0800

    Update PAT_A1102.cpp

--- Codes/PAT_A1102.cpp
+++ Codes/PAT_A1102.cpp
@@ -2,31 +2,11 @@
 #include <cstdio>
 #include <queue>
 using namespace std;
-int n, k, root;
+int n, notRoot[11] = {0};
 char l, r;
 struct node {
     int lchild = -1, rchild = -1;
-} Nodes[15];
-void invert(int root) {
-    if (root == -1)
-        return;
-    invert(Nodes[root].lchild);
-    invert(Nodes[root].rchild);
-    swap(Nodes[root].lchild, Nodes[root].rchild);
-    return;
-}
-int findRoot(int x) {
-    int flag = 0;
-    for (k = 0; k < n; k++)
-        if (Nodes[k].lchild == x || Nodes[k].rchild == x) {
-            flag = 1;
-            break;
-        }
-    if (flag)
-        return findRoot(k);
-    else
-        return x;
-}
+} Nodes[11];
 void levelOrder(int root) {
     int cnt = n;
     queue<int> q;
@@ -37,34 +17,39 @@ void levelOrder(int root) {
             printf(" ");
         else
             printf("\n");
-        if (Nodes[q.front()].lchild != -1)
-            q.push(Nodes[q.front()].lchild);
         if (Nodes[q.front()].rchild != -1)
             q.push(Nodes[q.front()].rchild);
+        if (Nodes[q.front()].lchild != -1)
+            q.push(Nodes[q.front()].lchild);
         q.pop();
     }
 }
 void inOrder(int root) {
     if (root == -1)
         return;
-    inOrder(Nodes[root].lchild);
+    inOrder(Nodes[root].rchild);
     printf("%d", root), n--;
     if (n)
         printf(" ");
     else
         printf("\n");
-    inOrder(Nodes[root].rchild);
+    inOrder(Nodes[root].lchild);
 }
 int main() {
     if (scanf("%d\n", &n))
         for (int i = 0; i < n; i++)
             if (scanf("%c %c\n", &l, &r)) {
-                Nodes[i].lchild = (l == '-') ? -1 : l - '0';
-                Nodes[i].rchild = (r == '-') ? -1 : r - '0';
+                Nodes[i].lchild = (l == '-') ? -1 : l - '0',
+                notRoot[l - '0'] = 1;
+                Nodes[i].rchild = (r == '-') ? -1 : r - '0',
+                notRoot[r - '0'] = 1;
+                ;
             }
-    root = findRoot(0);
-    invert(root);
-    levelOrder(root);
-    inOrder(root);
+    for (int i = 0; i < n; i++)
+        if (!notRoot[i]) {
+            levelOrder(i);
+            inOrder(i);
+            break;
+        }
     return 0;
 }
