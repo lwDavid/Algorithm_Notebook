4dadd60871ef31a6f8c86d394f7f5a1a96a99f60
David <47696114+lwDavid@users.noreply.github.com>
2022-04-15 12:26:42 +0800

    Update PAT_A1090.cpp

--- Codes/PAT_A1090.cpp
+++ Codes/PAT_A1090.cpp
@@ -48,39 +48,39 @@ int main() {
 }
 
 //The reference DFS method with concise codes.
-#include <cmath>
-#include <cstdio>
-#include <vector>
-using namespace std;
-const int maxn = 100010;
-vector<int> child[maxn];
-double p, r;
-int n, maxDepth = 0, num = 0;
-void DFS(int index, int depth) {
-    if (child[index].size() == 0) {
-        if (depth > maxDepth) {
-            maxDepth = depth;
-            num = 1;
-        } else if (depth == maxDepth)
-            num++;
-        return;
-    }
-    for (int i = 0; i < (int)child[index].size(); i++)
-        DFS(child[index][i], depth + 1);
-}
-int main() {
-    int father, root = -1;
-    if (scanf("%d %lf %lf", &n, &p, &r))
-        r /= 100;
-    for (int i = 0; i < n; i++) {
-        if (scanf("%d", &father))
-            ;
-        if (father != -1)
-            child[father].push_back(i);
-        else
-            root = i;
-    }
-    DFS(root, 0);
-    printf("%.2f %d\n", p * pow(1 + r, maxDepth), num);
-    return 0;
-}
+// #include <cmath>
+// #include <cstdio>
+// #include <vector>
+// using namespace std;
+// const int maxn = 100010;
+// vector<int> child[maxn];
+// double p, r;
+// int n, maxDepth = 0, num = 0;
+// void DFS(int index, int depth) {
+//     if (child[index].size() == 0) {
+//         if (depth > maxDepth) {
+//             maxDepth = depth;
+//             num = 1;
+//         } else if (depth == maxDepth)
+//             num++;
+//         return;
+//     }
+//     for (int i = 0; i < (int)child[index].size(); i++)
+//         DFS(child[index][i], depth + 1);
+// }
+// int main() {
+//     int father, root = -1;
+//     if (scanf("%d %lf %lf", &n, &p, &r))
+//         r /= 100;
+//     for (int i = 0; i < n; i++) {
+//         if (scanf("%d", &father))
+//             ;
+//         if (father != -1)
+//             child[father].push_back(i);
+//         else
+//             root = i;
+//     }
+//     DFS(root, 0);
+//     printf("%.2f %d\n", p * pow(1 + r, maxDepth), num);
+//     return 0;
+// }
