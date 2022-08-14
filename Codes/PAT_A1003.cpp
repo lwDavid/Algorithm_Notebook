a513b7db6f97c6330b6aa6cfda606ef6cd4ec213
David <47696114+lwDavid@users.noreply.github.com>
2022-08-14 22:03:08 +0800

    Update PAT_A1003.cpp

--- Codes/PAT_A1003.cpp
+++ Codes/PAT_A1003.cpp
@@ -41,3 +41,63 @@ int main() {
     printf("%d %d\n", num[c2], hands[c2]);
     return 0;
 }
+
+// The notebook version.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// using namespace std;
+// const int MAXV = 510;
+// const int INF = 1000000000;
+// int n, m, st, ed, G[MAXV][MAXV], weight[MAXV];
+// int d[MAXV], w[MAXV], num[MAXV];
+// bool vis[MAXV] = {false};
+// void Dijkstra(int s) {
+//     fill(d, d + MAXV, INF);
+//     memset(num, 0, sizeof(num));
+//     memset(w, 0, sizeof(w));
+//     d[s] = 0;
+//     w[s] = weight[s];
+//     num[s] = 1;
+//     for (int i = 0; i < n; i++) {
+//         int u = -1, MIN = INF;
+//         for (int j = 0; j < n; j++) {
+//             if (vis[j] == false && d[j] < MIN) {
+//                 u = j;
+//                 MIN = d[j];
+//             }
+//         }
+//         if (u == -1)
+//             return;
+//         vis[u] = true;
+//         for (int v = 0; v < n; v++) {
+//             if (vis[v] == false && G[u][v] != INF) {
+//                 if (d[u] + G[u][v] < d[v]) {
+//                     d[v] = d[u] + G[u][v];
+//                     w[v] = w[u] + weight[v];
+//                     num[v] = num[u];
+//                 } else if (d[u] + G[u][v] == d[v]) {
+//                     if (w[u] + weight[v] > w[v]) {
+//                         w[v] = w[u] + weight[v];
+//                     }
+//                     num[v] += num[u];
+//                 }
+//             }
+//         }
+//     }
+// }
+// int main() {
+//     if (scanf("%d%d%d%d", &n, &m, &st, &ed))
+//         for (int i = 0; i < n; i++)
+//             if (scanf("%d", &weight[i]))
+//                 ;
+//     int u, v;
+//     fill(G[0], G[0] + MAXV * MAXV, INF);
+//     for (int i = 0; i < m; i++)
+//         if (scanf("%d%d", &u, &v))
+//             if (scanf("%d", &G[u][v]))
+//                 G[v][u] = G[u][v];
+//     Dijkstra(st);
+//     printf("%d %d\n", num[ed], w[ed]);
+//     return 0;
+// }
