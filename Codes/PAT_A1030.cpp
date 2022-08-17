84693cd48ce4604ba543772126a62da04c3ec071
David <47696114+lwDavid@users.noreply.github.com>
2022-08-17 09:05:05 +0800

    Update PAT_A1030.cpp

--- Codes/PAT_A1030.cpp
+++ Codes/PAT_A1030.cpp
@@ -1,4 +1,4 @@
-// My initial version.
+// My initial version with better complexity.
 #include <algorithm>
 #include <cstdio>
 using namespace std;
@@ -109,5 +109,80 @@ int main() {
 //     Dijkstra(st);
 //     DFS(ed);
 //     printf("%d %d\n", d[ed], c[ed]);
+//     return 0;
+// }
+
+// Notebook versing using Dijkstra & DFS.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// #include <vector>
+// using namespace std;
+// const int MAXV = 510;
+// const int INF = 1000000000;
+// int n, m, st, ed, G[MAXV][MAXV], cost[MAXV][MAXV];
+// int d[MAXV], minCost = INF;
+// bool vis[MAXV] = {false};
+// vector<int> pre[MAXV];
+// vector<int> tempPath, path;
+// void Dijkstra(int s) {
+//     fill(d, d + MAXV, INF);
+//     d[s] = 0;
+//     for (int i = 0; i < n; i++) {
+//         int u = -1, MIN = INF;
+//         for (int j = 0; j < n; j++)
+//             if (vis[j] == false && d[j] < MIN) {
+//                 u = j;
+//                 MIN = d[j];
+//             }
+//         if (u == -1)
+//             return;
+//         vis[u] = true;
+//         for (int v = 0; v < n; v++) {
+//             if (vis[v] == false && G[u][v] != INF) {
+//                 if (d[u] + G[u][v] < d[v]) {
+//                     d[v] = d[u] + G[u][v];
+//                     pre[v].clear();
+//                     pre[v].push_back(u);
+//                 } else if (d[u] + G[u][v] == d[v])
+//                     pre[v].push_back(u);
+//             }
+//         }
+//     }
+// }
+// void DFS(int v) {
+//     if (v == st) {
+//         tempPath.push_back(v);
+//         int tempCost = 0;
+//         for (int i = tempPath.size() - 1; i > 0; i--) {
+//             int id = tempPath[i], idNext = tempPath[i - 1];
+//             tempCost += cost[id][idNext];
+//         }
+//         if (tempCost < minCost) {
+//             minCost = tempCost;
+//             path = tempPath;
+//         }
+//         tempPath.pop_back();
+//         return;
+//     }
+//     tempPath.push_back(v);
+//     for (int i = 0; i < (int)pre[v].size(); i++)
+//         DFS(pre[v][i]);
+//     tempPath.pop_back();
+// }
+// int main() {
+//     if (scanf("%d%d%d%d", &n, &m, &st, &ed))
+//         fill(G[0], G[0] + MAXV * MAXV, INF);
+//     int u, v;
+//     fill(cost[0], cost[0] + MAXV * MAXV, INF);
+//     for (int i = 0; i < m; i++)
+//         if (scanf("%d%d", &u, &v))
+//             if (scanf("%d%d", &G[u][v], &cost[u][v]))
+//                 G[v][u] = G[u][v], cost[v][u] = cost[u][v];
+//     Dijkstra(st);
+//     DFS(ed);
+//     for (int i = path.size() - 1; i >= 0; i--)
+//         printf("%d ", path[i]);
+//     printf("%d %d\n", d[ed], minCost);
 //     return 0;
 // }
