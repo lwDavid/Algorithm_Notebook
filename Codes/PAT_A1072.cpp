ec3cae43a959740b0288ae558a68aeece8177ec6
David <47696114+lwDavid@users.noreply.github.com>
2022-08-19 06:13:04 +0800

    Update PAT_A1072.cpp

--- Codes/PAT_A1072.cpp
+++ Codes/PAT_A1072.cpp
@@ -1,4 +1,4 @@
-// My initial version using Dijkstra.
+// My initial version using Dijkstra and adjacency list, has better complexities in average.
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
@@ -85,3 +85,90 @@ int main() {
     }
     return 0;
 }
+
+// The notebook version, using Dijkstra and adjacency matrix without sort(), faster only in large scale data.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// using namespace std;
+// const int MAXV = 1020;
+// const int INF = 1000000000;
+// int n, m, k, DS, G[MAXV][MAXV];
+// int d[MAXV];
+// bool vis[MAXV] = {false};
+// void Dijkstra(int s) {
+//     memset(vis, false, sizeof(vis));
+//     fill(d, d + MAXV, INF);
+//     d[s] = 0;
+//     for (int i = 0; i < n + m; i++) {
+//         int u = -1, MIN = INF;
+//         for (int j = 1; j <= n + m; j++)
+//             if (vis[j] == false && d[j] < MIN) {
+//                 u = j;
+//                 MIN = d[j];
+//             }
+//         if (u == -1)
+//             return;
+//         vis[u] = true;
+//         for (int v = 1; v <= n + m; v++)
+//             if (vis[v] == false && G[u][v] != INF)
+//                 if (d[u] + G[u][v] < d[v])
+//                     d[v] = d[u] + G[u][v];
+//     }
+// }
+// int getID(char str[]) {
+//     int i = 0, len = strlen(str), ID = 0;
+//     while (i < len) {
+//         if (str[i] != 'G')
+//             ID = ID * 10 + (str[i] - '0');
+//         i++;
+//     }
+//     if (str[0] == 'G')
+//         return n + ID;
+//     else
+//         return ID;
+// }
+// int main() {
+//     int u, v, w;
+//     char city1[5], city2[5];
+//     if (scanf("%d%d%d%d", &n, &m, &k, &DS))
+//         fill(G[0], G[0] + MAXV * MAXV, INF);
+//     for (int i = 0; i < k; i++)
+//         if (scanf("%s %s %d", city1, city2, &w)) {
+//             u = getID(city1);
+//             v = getID(city2);
+//             G[u][v] = G[v][u] = w;
+//         }
+//     double ansDis = -1, ansAvg = INF;
+//     int ansID = -1;
+//     for (int i = n + 1; i <= n + m; i++) {
+//         double minDis = INF, avg = 0;
+//         Dijkstra(i);
+//         for (int j = 1; j <= n; j++) {
+//             if (d[j] > DS) {
+//                 minDis = -1;
+//                 break;
+//             }
+//             if (d[j] < minDis)
+//                 minDis = d[j];
+//             avg += 1.0 * d[j] / n;
+//         }
+//         if (minDis == -1)
+//             continue;
+//         if (minDis > ansDis) {
+//             ansID = i;
+//             ansDis = minDis;
+//             ansAvg = avg;
+//         } else if (minDis == ansDis && avg < ansAvg) {
+//             ansID = i;
+//             ansAvg = avg;
+//         }
+//     }
+//     if (ansID == -1)
+//         printf("No Solution\n");
+//     else {
+//         printf("G%d\n", ansID - n);
+//         printf("%.1f %.1f", ansDis, ansAvg);
+//     }
+//     return 0;
+// }
