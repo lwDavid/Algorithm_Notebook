eee5d62ab0370f334b0982fc93320a45c32d449b
David <47696114+lwDavid@users.noreply.github.com>
2022-08-24 07:22:45 +0800

    Update PAT_A1045.cpp

--- Codes/PAT_A1045.cpp
+++ Codes/PAT_A1045.cpp
@@ -24,3 +24,37 @@ int main() {
     printf("%d\n", t);
     return 0;
 }
+
+// The notebook version with a better memory consume.
+// #include <algorithm>
+// #include <cstdio>
+// #include <cstring>
+// using namespace std;
+// const int maxc = 210;
+// const int maxn = 10010;
+// int HashTable[maxc];
+// int A[maxn], dp[maxn];
+// int main() {
+//     int n, m, x;
+//     if (scanf("%d%d", &n, &m))
+//         memset(HashTable, -1, sizeof(HashTable));
+//     for (int i = 0; i < m; i++)
+//         if (scanf("%d", &x))
+//             HashTable[x] = i;
+//     int L, num = 0;
+//     if (scanf("%d", &L))
+//         for (int i = 0; i < L; i++)
+//             if (scanf("%d", &x))
+//                 if (HashTable[x] >= 0)
+//                     A[num++] = HashTable[x];
+//     int ans = -1;
+//     for (int i = 0; i < num; i++) {
+//         dp[i] = 1;
+//         for (int j = 0; j < i; j++)
+//             if (A[j] <= A[i] && dp[i] < dp[j] + 1)
+//                 dp[i] = dp[j] + 1;
+//         ans = max(ans, dp[i]);
+//     }
+//     printf("%d\n", ans);
+//     return 0;
+// }
