3f8ba62295d13aa63db6f4923dea957835e6f30e
David <47696114+lwDavid@users.noreply.github.com>
2022-08-24 06:01:11 +0800

    Update PAT_A1007.cpp

--- Codes/PAT_A1007.cpp
+++ Codes/PAT_A1007.cpp
@@ -1,4 +1,4 @@
-// My initial version.
+// My initial version with better complexities.
 #include <algorithm>
 #include <cstdio>
 using namespace std;
@@ -25,3 +25,38 @@ int main() {
     }
     return 0;
 }
+
+// The notebook version.
+// #include <cstdio>
+// const int maxn = 10010;
+// int a[maxn], dp[maxn];
+// int s[maxn] = {0};
+// int main() {
+//     int n;
+//     bool flag = false;
+//     if (scanf("%d", &n))
+//         for (int i = 0; i < n; i++)
+//             if (scanf("%d", &a[i]))
+//                 if (a[i] >= 0)
+//                     flag = true;
+//     if (flag == false) {
+//         printf("0 %d %d\n", a[0], a[n - 1]);
+//         return 0;
+//     }
+//     dp[0] = a[0];
+//     for (int i = 1; i < n; i++) {
+//         if (dp[i - 1] + a[i] > a[i]) {
+//             dp[i] = dp[i - 1] + a[i];
+//             s[i] = s[i - 1];
+//         } else {
+//             dp[i] = a[i];
+//             s[i] = i;
+//         }
+//     }
+//     int k = 0;
+//     for (int i = 1; i < n; i++)
+//         if (dp[i] > dp[k])
+//             k = i;
+//     printf("%d %d %d\n", dp[k], a[s[k]], a[k]);
+//     return 0;
+// }
