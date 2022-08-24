31b109221fda1ae80f2181ae7720888f48301bbb
David <47696114+lwDavid@users.noreply.github.com>
2022-08-24 08:17:26 +0800

    Update PAT_A1045.cpp

--- Codes/PAT_A1045.cpp
+++ Codes/PAT_A1045.cpp
@@ -58,3 +58,37 @@ int main() {
 //     printf("%d\n", ans);
 //     return 0;
 // }
+
+// The notebook version using LCS(Longest Common Subsequence) instead of LIS(Longest Increasing Subsequence), which is faster but consume more memory.
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+const int maxc = 210;
+const int maxn = 10010;
+int A[maxc], B[maxn], dp[maxc][maxn];
+int main() {
+    int n, m;
+    if (scanf("%d%d", &n, &m))
+        for (int i = 1; i <= m; i++)
+            if (scanf("%d", &A[i]))
+                ;
+    int L;
+    if (scanf("%d", &L))
+        for (int i = 1; i <= L; i++)
+            if (scanf("%d", &B[i]))
+                ;
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    for (int j = 0; j <= L; j++)
+        dp[0][j] = 0;
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= L; j++) {
+            int MAX = max(dp[i - 1][j], dp[i][j - 1]);
+            if (A[i] == B[j])
+                dp[i][j] = MAX + 1;
+            else
+                dp[i][j] = MAX;
+        }
+    printf("%d\n", dp[m][L]);
+    return 0;
+}
