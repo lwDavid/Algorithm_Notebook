14cf6cd995982d4a413dc9661a1ae4f86cd003d5
David <lw.guo@outlook.com>
2023-01-16 06:36:25 +0800

    Update PAT_A1040.cpp

--- Codes/PAT_A1040.cpp
+++ Codes/PAT_A1040.cpp
@@ -38,3 +38,58 @@ int main() {
 
     return 0;
 }
+
+// O(n) Manacher algorithm
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+const int maxn = 2600;
+
+int main() {
+    char str_[maxn], news[maxn];
+    int len = 0, index = 0, p[maxn] = {0};
+
+    // Initialize the new string
+    if (fgets(str_, maxn, stdin)) {
+        len = strlen(str_);
+        news[index++] = '@';
+        for (int i = 0; i < len; i++) {
+            news[index++] = '#';
+            news[index++] = str_[i];
+        }
+        news[index] = 0;
+        len = strlen(news);
+    }
+
+    int right = 0, center = 0, maxlen = 1;
+
+    // Core algorithm
+    for (int i = 0; i < len; i++) {
+        int mirror = 2 * center - 1;  // Find mirror position
+        p[i] = (i >= right)
+                   ? 1
+                   : min(right - i, p[mirror]);  // Calculate radius of i
+
+        // Expand radius at i
+        while (news[i + p[i]] == news[i - p[i]]) {
+            p[i]++;
+        }
+
+        // Calculate new center and right bound
+        if (p[i] + i > right) {
+            right = p[i] + i;
+            center = i;
+        }
+
+        // Record the max symmetric value
+        if (maxlen < p[i]) {
+            maxlen = p[i];
+        }
+    }
+
+    printf("%d\n", maxlen - 1);
+
+    return 0;
+}
